69726f93cf4c360cb0d3750b81331c5715bf8332
axel-ac <abdullahceylan.axel@gmail.com>
2023-07-06 10:24:53 +0300

    finished

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,11 @@
-import logo from "../img/logo.png";
+
 import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-md navbar-light">
       <div className="container-fluid">
-        <Link to="/" className="navbar-brand">
-          <img src={logo} alt="" />
-        </Link>
+      
         <button
           className="navbar-toggler"
           type="button"
--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,5 +1,19 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+
+//? Link, NavLink ve Navigate componentleri declarative routing
+//? gerceklestirmek icin kullanilir.
+//? Ornegin: Link ve NavLink Sayfada gorulebilen, kullanciyla
+//? bir etkilesim icerisinde bulunarak yonledirme yapilan bir
+//? componentlerdir. (Nav v.b)
+
+//? Navigate componenti sayfada gorulmeyen ve programsal olarak bir linkin
+//? bir baska linke yonledirilmesi icin kullanilabilir. (v5 -> Redirect)
+//? Navigate, Component seviyesi Routing icin kullanilir.
+
+//* useNavigate() ise imperative routing icin elverislidir.
+//* Ornegin bir fonksiyon,event veye UseEffect icerisinde programsal
+//* olarak yonledirme yapmak icin kullanilabilir.
 
 const People = () => {
   const [people, setPeople] = useState([]);
@@ -19,8 +33,8 @@ const People = () => {
     <div className="container text-center mt-4">
       <h1>PEOPLE LIST</h1>
       <div className="row justify-content-center g-3">
-        {people?.map((person) => {
-          const { id, first_name, last_name, avatar } = person;
+        {people?.map((p) => {
+          const { id, first_name, last_name, avatar } = p;
           return (
             <div
               key={id}
@@ -28,13 +42,16 @@ const People = () => {
               type="button"
               //? Absolute path (tam adres vermek)
               // onClick={() => navigate(`/people/${id}`)}
-              //! Relative path (goreceli adres vermek, nested olarak ilerliyorsak bu avantajlı)
-              onClick={() => navigate(`${id}`, {state: person})}
+              //! Relative path (goreceli adres vermek)
+              onClick={() => navigate(`${id}`, { state: p })}
             >
               <img className="rounded" src={avatar} alt="img" />
               <h6>
                 {first_name} {last_name}
               </h6>
+
+              {/* <Link to={`/people/${id}`}> Deneme</Link>
+              <Navigate to={`/people/${id}`} /> */}
             </div>
           );
         })}
